2ec9d2f22339e81ac6d2b18f16c212184eb0cd44
Sandro <33160424+ElSanders@users.noreply.github.com>
2020-10-25 17:16:46 -0500

    Added MariaDB Functions

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http = require('http')
+const mariadb = require('mariadb');
 const fs = require('fs')
 const port = 3000
+const pool = mariadb.createPool({
+    host: 'localhost',
+    port: '3306',
+    database: 'sandro', 
+    user:'root', 
+    password: 'password',
+    connectionLimit: 5
+});
+pool.getConnection()
+    .then(conn => {
+    
+      conn.query("SELECT d FROM texto")
+        .then((rows) => {
+          var string = rows[0];
+          console.log(string)
 
+        })
+        .then((res) => {
+          //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err); 
+          conn.end();
+        })
+        
+    }).catch(err => {
+      //not connected
+    });
 const server = http.createServer(function(req, res){
  res.writeHead(200, {'Content-Type': 'text/html'})
  fs.readFile('index.html',function(error,data){
@@ -17,8 +47,8 @@ const server = http.createServer(function(req, res){
 
 server.listen(port, function(error){
     if(error){
-        console.log('something went wrong', error)
+        console.log('Something Went Wrong', error)
     } else {
-        console.log('serve is listening on port '+port)
+        console.log('Server Active and Listening on Port '+port)
     }
 })
